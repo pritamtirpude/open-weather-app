0ed10ea65205d2f178f65f32f89e99b8733de486
Pritam tirpude <pritam.tirpude9601@gmail.com>
2025-09-09 22:30:56 +0530

    create UI for weather app using static dummy data, Figma design from frontendmentor.io

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,19 +1,22 @@
 export default function SearchInput() {
   return (
-    <div className="mx-auto mt-12 w-xl">
-      <form onSubmit={(e) => e.preventDefault()} className="relative flex items-center gap-x-4">
+    <div className="mx-auto mt-12 w-[656px] lg:mt-16">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="relative flex flex-col items-center gap-3 md:flex-row md:gap-4"
+      >
         <img
-          className="absolute top-1/2 left-4 -translate-y-1/2 transform"
+          className="absolute top-1/2 left-6 -translate-y-1/2 transform"
           src="/assets/images/icon-search.svg"
           alt="Search icon"
         />
         <input
-          className="bg-weather-700 text-weather-200 focus:outline-weather-200 placeholder:text-weather-200 font-dm-sans w-full rounded-2xl px-12 py-3.5 focus:outline-2 focus:outline-offset-2"
+          className="bg-weather-700 text-weather-200 focus:outline-weather-200 placeholder:text-weather-200 font-dm-sans w-full rounded-xl px-14 py-4 focus:outline-2 focus:outline-offset-2"
           type="text"
           placeholder="Search for a place..."
         />
         <button
-          className="bg-weather-blue-500 focus:outline-weather-blue-700 hover:bg-weather-blue-700 text-weather-200 font-dm-sans cursor-pointer rounded-lg px-6 py-3 font-semibold duration-150 focus:outline-2 focus:outline-offset-2"
+          className="bg-weather-blue-500 focus:outline-weather-blue-700 hover:bg-weather-blue-700 font-dm-sans w-full cursor-pointer rounded-xl px-6 py-4 font-semibold text-white duration-150 focus:outline-2 focus:outline-offset-2 md:w-auto"
           type="submit"
         >
           Search
--- src/components/WeatherGrid/WeatherGrid.tsx
+++ src/components/WeatherGrid/WeatherGrid.tsx
@@ -1,114 +1,116 @@
 export default function WeatherGrid() {
   return (
-    <section className="mt-6 grid grid-cols-3 gap-6">
+    <section className="mt-8 grid grid-cols-3 gap-8 lg:mt-12">
       <div className="col-span-2">
-        <div className="flex size-full items-center justify-between bg-[url(/assets/images/bg-today-large.svg)] bg-cover bg-center bg-no-repeat px-6 py-20">
+        <div className="flex h-[286px] items-center justify-between rounded-[20px] bg-[url(/assets/images/bg-today-large.svg)] bg-cover bg-center bg-no-repeat px-6">
           <div>
-            <h1 className="font-dm-sans text-weather-200 text-3xl font-bold">Berlin, Germany</h1>
-            <span className="font-dm-sans text-weather-300 text-base">Tuesday, Aug, 2025</span>
+            <h1 className="font-dm-sans text-dm-sans-preset-3 text-white">Berlin, Germany</h1>
+            <span className="font-dm-sans text-dm-sans-preset-6 text-white/80">
+              Tuesday, Aug, 2025
+            </span>
           </div>
-          <div className="flex items-center">
+          <div className="flex w-[294px] items-center gap-5">
             <img
               src="/assets/images/icon-sunny.webp"
-              className="size-28 object-cover"
+              className="size-[120px]"
               alt="location icon"
             />
-            <span className="font-dm-sans text-weather-200 text-8xl font-semibold italic">68°</span>
+            <span className="font-dm-sans text-dm-sans-preset-1 text-white italic">20°</span>
           </div>
         </div>
       </div>
       <div className="col-span-2 grid size-full grid-cols-4 gap-6">
-        <div className="bg-weather-700 border-weather-300/15 flex flex-col gap-y-3 rounded-2xl border p-6">
-          <h2 className="font-dm-sans text-weather-200 text-base">Feels Like</h2>
-          <span className="font-dm-sans text-weather-200 text-3xl">64°</span>
+        <div className="bg-weather-800 border-weather-600 flex flex-col gap-y-3 rounded-xl border p-5">
+          <h2 className="font-dm-sans text-weather-200 text-dm-sans-preset-6">Feels Like</h2>
+          <span className="font-dm-sans text-dm-sans-preset-2 text-white">64°</span>
         </div>
 
-        <div className="bg-weather-700 border-weather-300/15 flex flex-col gap-y-3 rounded-2xl border p-6">
-          <h2 className="font-dm-sans text-weather-200 text-base">Humidity</h2>
-          <span className="font-dm-sans text-weather-200 text-3xl">46%</span>
+        <div className="bg-weather-800 border-weather-600 flex flex-col gap-y-3 rounded-xl border p-5">
+          <h2 className="font-dm-sans text-weather-200 text-dm-sans-preset-6">Humidity</h2>
+          <span className="font-dm-sans text-dm-sans-preset-2 text-white">46%</span>
         </div>
 
-        <div className="bg-weather-700 border-weather-300/15 flex flex-col gap-y-3 rounded-2xl border p-6">
-          <h2 className="font-dm-sans text-weather-200 text-base">Wind</h2>
-          <span className="font-dm-sans text-weather-200 text-3xl">9 mph</span>
+        <div className="bg-weather-800 border-weather-600 flex flex-col gap-y-3 rounded-xl border p-5">
+          <h2 className="font-dm-sans text-weather-200 text-dm-sans-preset-6">Wind</h2>
+          <span className="font-dm-sans text-dm-sans-preset-2 text-white">9 mph</span>
         </div>
 
-        <div className="bg-weather-700 border-weather-300/15 flex flex-col gap-y-3 rounded-2xl border p-6">
-          <h2 className="font-dm-sans text-weather-200 text-base">Precipitation</h2>
-          <span className="font-dm-sans text-weather-200 text-3xl">0 in</span>
+        <div className="bg-weather-800 border-weather-600 flex flex-col gap-y-3 rounded-xl border p-5">
+          <h2 className="font-dm-sans text-weather-200 text-dm-sans-preset-6">Precipitation</h2>
+          <span className="font-dm-sans text-dm-sans-preset-2 text-white">0 in</span>
         </div>
       </div>
-      <div className="col-span-2 col-start-1">
-        <h2 className="text-weather-200 font-dm-sans text-xl font-bold">Daily forecast</h2>
+      <div className="col-span-2 col-start-1 mt-12">
+        <h2 className="font-dm-sans text-dm-sans-preset-4 text-white">Daily forecast</h2>
 
-        <div className="mt-6 flex items-center gap-4">
-          <div className="bg-weather-700 border-weather-300/15 flex w-full flex-col items-center justify-center gap-4 rounded-2xl p-4">
-            <h3 className="font-dm-sans text-weather-200 text-base">Tue</h3>
-            <img className="size-14" src="/assets/images/icon-rain.webp" alt="icon rain" />
+        <div className="mt-5 flex items-center gap-4">
+          <div className="bg-weather-800 border-weather-600 flex w-full flex-col items-center justify-center gap-4 rounded-xl px-2.5 py-4">
+            <h3 className="font-dm-sans text-dm-sans-preset-6 text-white">Tue</h3>
+            <img className="size-[60px]" src="/assets/images/icon-rain.webp" alt="icon rain" />
 
             <div className="flex size-full items-center justify-between">
-              <span className="text-weather-200 font-dm-sans text-base font-bold">68°</span>
-              <span className="text-weather-200 font-dm-sans text-base">57°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">68°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">57°</span>
             </div>
           </div>
 
-          <div className="bg-weather-700 border-weather-300/15 flex w-full flex-col items-center justify-center gap-4 rounded-2xl p-4">
-            <h3 className="font-dm-sans text-weather-200 text-base">Tue</h3>
-            <img className="size-14" src="/assets/images/icon-rain.webp" alt="icon rain" />
+          <div className="bg-weather-800 border-weather-600 flex w-full flex-col items-center justify-center gap-4 rounded-xl px-2.5 py-4">
+            <h3 className="font-dm-sans text-dm-sans-preset-6 text-white">Tue</h3>
+            <img className="size-[60px]" src="/assets/images/icon-rain.webp" alt="icon rain" />
 
             <div className="flex size-full items-center justify-between">
-              <span className="text-weather-200 font-dm-sans text-base font-bold">68°</span>
-              <span className="text-weather-200 font-dm-sans text-base">57°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">68°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">57°</span>
             </div>
           </div>
 
-          <div className="bg-weather-700 border-weather-300/15 flex w-full flex-col items-center justify-center gap-4 rounded-2xl p-4">
-            <h3 className="font-dm-sans text-weather-200 text-base">Tue</h3>
-            <img className="size-14" src="/assets/images/icon-rain.webp" alt="icon rain" />
+          <div className="bg-weather-800 border-weather-600 flex w-full flex-col items-center justify-center gap-4 rounded-xl px-2.5 py-4">
+            <h3 className="font-dm-sans text-dm-sans-preset-6 text-white">Tue</h3>
+            <img className="size-[60px]" src="/assets/images/icon-rain.webp" alt="icon rain" />
 
             <div className="flex size-full items-center justify-between">
-              <span className="text-weather-200 font-dm-sans text-base font-bold">68°</span>
-              <span className="text-weather-200 font-dm-sans text-base">57°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">68°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">57°</span>
             </div>
           </div>
 
-          <div className="bg-weather-700 border-weather-300/15 flex w-full flex-col items-center justify-center gap-4 rounded-2xl p-4">
-            <h3 className="font-dm-sans text-weather-200 text-base">Tue</h3>
-            <img className="size-14" src="/assets/images/icon-rain.webp" alt="icon rain" />
+          <div className="bg-weather-800 border-weather-600 flex w-full flex-col items-center justify-center gap-4 rounded-xl px-2.5 py-4">
+            <h3 className="font-dm-sans text-dm-sans-preset-6 text-white">Tue</h3>
+            <img className="size-[60px]" src="/assets/images/icon-rain.webp" alt="icon rain" />
 
             <div className="flex size-full items-center justify-between">
-              <span className="text-weather-200 font-dm-sans text-base font-bold">68°</span>
-              <span className="text-weather-200 font-dm-sans text-base">57°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">68°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">57°</span>
             </div>
           </div>
 
-          <div className="bg-weather-700 border-weather-300/15 flex w-full flex-col items-center justify-center gap-4 rounded-2xl p-4">
-            <h3 className="font-dm-sans text-weather-200 text-base">Tue</h3>
-            <img className="size-14" src="/assets/images/icon-rain.webp" alt="icon rain" />
+          <div className="bg-weather-800 border-weather-600 flex w-full flex-col items-center justify-center gap-4 rounded-xl px-2.5 py-4">
+            <h3 className="font-dm-sans text-dm-sans-preset-6 text-white">Tue</h3>
+            <img className="size-[60px]" src="/assets/images/icon-rain.webp" alt="icon rain" />
 
             <div className="flex size-full items-center justify-between">
-              <span className="text-weather-200 font-dm-sans text-base font-bold">68°</span>
-              <span className="text-weather-200 font-dm-sans text-base">57°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">68°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">57°</span>
             </div>
           </div>
 
-          <div className="bg-weather-700 border-weather-300/15 flex w-full flex-col items-center justify-center gap-4 rounded-2xl p-4">
-            <h3 className="font-dm-sans text-weather-200 text-base">Tue</h3>
-            <img className="size-14" src="/assets/images/icon-rain.webp" alt="icon rain" />
+          <div className="bg-weather-800 border-weather-600 flex w-full flex-col items-center justify-center gap-4 rounded-xl px-2.5 py-4">
+            <h3 className="font-dm-sans text-dm-sans-preset-6 text-white">Tue</h3>
+            <img className="size-[60px]" src="/assets/images/icon-rain.webp" alt="icon rain" />
 
             <div className="flex size-full items-center justify-between">
-              <span className="text-weather-200 font-dm-sans text-base font-bold">68°</span>
-              <span className="text-weather-200 font-dm-sans text-base">57°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">68°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">57°</span>
             </div>
           </div>
 
-          <div className="bg-weather-700 border-weather-300/15 flex w-full flex-col items-center justify-center gap-4 rounded-2xl p-4">
-            <h3 className="font-dm-sans text-weather-200 text-base">Tue</h3>
-            <img className="size-14" src="/assets/images/icon-rain.webp" alt="icon rain" />
+          <div className="bg-weather-800 border-weather-600 flex w-full flex-col items-center justify-center gap-4 rounded-xl px-2.5 py-4">
+            <h3 className="font-dm-sans text-dm-sans-preset-6 text-white">Tue</h3>
+            <img className="size-[60px]" src="/assets/images/icon-rain.webp" alt="icon rain" />
 
             <div className="flex size-full items-center justify-between">
-              <span className="text-weather-200 font-dm-sans text-base font-bold">68°</span>
-              <span className="text-weather-200 font-dm-sans text-base">57°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">68°</span>
+              <span className="font-dm-sans text-dm-sans-preset-7 text-white">57°</span>
             </div>
           </div>
         </div>
